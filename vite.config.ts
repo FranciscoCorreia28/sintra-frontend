f47e3a0ad4c285ebddeaa5be730173fd360275db
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-24 09:48:36 +0100

    deploy divided modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 30000,
-
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
+      }
+  }
   },
   plugins: [react()]
 })
