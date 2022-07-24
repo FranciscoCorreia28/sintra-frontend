6695e7d748485c1ac0572be00fa0235663a173e0
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-24 09:34:48 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/deploying-vite-project-example/",
+  build: {
+    chunkSizeWarningLimit: 30000,
+
+  },
   plugins: [react()]
 })
